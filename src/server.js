67067faeba028536138c0f9315b0aedca459fcac
haesooKr <gotn676@gmail.com>
2020-02-01 18:17:05 -0500

    Refactoring schema

--- src/server.js
+++ src/server.js
@@ -1,23 +1,12 @@
 require('dotenv').config();
 import { GraphQLServer } from 'graphql-yoga';
 import logger from 'morgan';
+import schema from './schema';
 
 const PORT = process.env.PORT || 4000;
 
-const typeDefs = `
-type Query{
-  hello: String!
-}
-`;
-
-const resolvers = {
-  Query:{
-    hello: () => "Hi"
-  } 
-}
-
 const server = new GraphQLServer({
-  typeDefs, resolvers
+  schema
 })
 server.express.use(logger("dev"));
 server.start({ port: PORT }, () => console.log(`Server running on https://localhost:${PORT}`));
